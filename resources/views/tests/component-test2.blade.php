0d9acd863829c570639ded5ed9267503003c347d
japangaiai <43401204+japangaiai@users.noreply.github.com>
2022-01-10 10:44:38 +0900

    classBase

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,4 +1,7 @@
 <x-tests.app>
     <x-slot name="header">ヘッダー2</x-slot>
 コンポーネントテスト２
+<x-test-class-base classBaseMessage="メッセージです" />
+<br>
+<x-test-class-base classBaseMessage="メッセージです" defaultMessage="初期値から変更" />
 </x-tests.app>
